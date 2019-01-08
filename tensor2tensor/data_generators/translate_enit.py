1c72b043e48637d2e328a505927081a68e86203f
Erra-Ernesto <46380181+Erra-Ernesto@users.noreply.github.com>
2019-01-08 17:12:49 +0100

    Update translate_enit.py

--- tensor2tensor/data_generators/translate_enit.py
+++ tensor2tensor/data_generators/translate_enit.py
@@ -58,9 +58,9 @@ _ENIT_TRAIN_DATASETS = [
 
 _ENIT_TEST_DATASETS = [
     [
-        "http://download1869.mediafire.com/4728hs18i7kg/yibn5iibdy4t6yk/News-Commentary11.tar",  # Newscommentary11
-        ("News-Commentary11/News-Commentary11.en-it.en",
-         "News-Commentary11/News-Commentary11.en-it.it")
+        "http://download1869.mediafire.com/4728hs18i7kg/yibn5iibdy4t6yk/News-Commentary.tar",  # Newscommentary11
+        ("News-Commentary/News-Commentary.en-it.en",
+         "News-Commentary/News-Commentary.en-it.it")
     ],
     [
         "http://dw.convertfiles.com/files/0237482001525345058/OpenSubtitles.tar",
