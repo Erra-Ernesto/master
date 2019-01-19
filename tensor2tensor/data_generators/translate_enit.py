801d548f4f9bc68dedb9585191fb32282c303fea
Erra-Ernesto <46380181+Erra-Ernesto@users.noreply.github.com>
2019-01-19 13:01:09 +0100

    Update translate_enit.py

--- tensor2tensor/data_generators/translate_enit.py
+++ tensor2tensor/data_generators/translate_enit.py
@@ -39,11 +39,11 @@ _ENIT_TRAIN_DATASETS = [
         "http://download1193.mediafire.com/0ljw8tp9q76g/1pwrjzcmtok4zwo/ParaCrawl.tar",  # ParaCrawl
         ("ParaCrawl/ParaCrawl.en-it.en",
          "ParaCrawl/ParaCrawl.en-it.it")
-#    ],
-#    [
-#        "http://download1193.mediafire.com/0ljw8tp9q76g/1pwrjzcmtok4zwo/Europarl_v7.tar",  # Europarl_v7
-#        ("Europarl_v7/Europarl.en-it.en",
-#         "Europarl_v7/Europarl.en-it.it")
+    ],
+    [
+        "http://download1193.mediafire.com/0ljw8tp9q76g/1pwrjzcmtok4zwo/Europarl_v7.tar",  # Europarl_v7
+        ("Europarl_v7/Europarl.en-it.en",
+         "Europarl_v7/Europarl.en-it.it")
     ]
 ]
 
