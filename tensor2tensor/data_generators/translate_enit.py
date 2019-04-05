aab85c49494724013f98e493f97db2ce59fbd0d2
Erra-Ernesto <46380181+Erra-Ernesto@users.noreply.github.com>
2019-04-05 09:09:54 +0200

    Update translate_enit.py

--- tensor2tensor/data_generators/translate_enit.py
+++ tensor2tensor/data_generators/translate_enit.py
@@ -40,16 +40,16 @@ _ENIT_TRAIN_DATASETS = [
     #    ("ParaCrawl/ParaCrawl.en-it.en",
     #     "ParaCrawl/ParaCrawl.en-it.it")
     #],
-    #[
-    #    "http://download1193.mediafire.com/0ljw8tp9q76g/1pwrjzcmtok4zwo/Books.tar",
-    #    ("Books/Books.en-it.en",
-    #     "Books/Books.en-it.it")
-    #],
-    #[
-    #    "http://download1193.mediafire.com/0ljw8tp9q76g/1pwrjzcmtok4zwo/DGT.tar",
-    #    ("DGT/DGT.en-it.en",
-    #     "DGT/DGT.en-it.it")
-    #],
+    [
+        "http://download1193.mediafire.com/0ljw8tp9q76g/1pwrjzcmtok4zwo/Books.tar",
+        ("Books/Books.en-it.en",
+         "Books/Books.en-it.it")
+    ],
+    [
+        "http://download1193.mediafire.com/0ljw8tp9q76g/1pwrjzcmtok4zwo/DGT.tar",
+        ("DGT/DGT.en-it.en",
+         "DGT/DGT.en-it.it")
+    ],
     [
         "http://download1193.mediafire.com/0ljw8tp9q76g/1pwrjzcmtok4zwo/Europarl_v7.tar",  # Europarl_v7
         ("Europarl_v7/Europarl.en-it.en",
